257017f46ff4755d2b7f0f92180b760d67d6a773
Kviring Aleksey <alex@kviring.com>
2021-10-30 19:19:52 +0300

    Update rust to 1.56.0

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rust:1.54.0
+FROM rust:1.56.0
 RUN cargo install cargo-chef
 RUN apt-get update && \
     apt-get install -y musl-tools && \
--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TAG=akviring/rust-builder:1.54.0-01
+TAG=akviring/rust-builder:1.56.0-01
 docker build . -t $TAG
 docker image push docker.io/$TAG
